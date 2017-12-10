d15b63fe8ff2af688d13f649c2643561ad336e4a
Carlos Alberto <carmantillab@gmail.com>
2017-12-10 16:27:14 -0500

    Formulario para nuevo usuario con controlador listo

--- src/main/java/com/cmancode/clientes/app/controllers/ClienteController.java
+++ src/main/java/com/cmancode/clientes/app/controllers/ClienteController.java
@@ -1,7 +1,5 @@
 package com.cmancode.clientes.app.controllers;
 
-import javax.validation.metadata.MethodType;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.cmancode.clientes.app.dao.IClienteDao;
+import com.cmancode.clientes.app.model.Cliente;
 
 @Controller
 public class ClienteController {
@@ -21,5 +20,15 @@ public class ClienteController {
 		model.addAttribute("titulo", "Lista de clientes");
 		model.addAttribute("clientes", clienteDao.findAll());
 		return "clientes";
+	}
+	
+	@RequestMapping(value = "/cliente", method = RequestMethod.GET)
+	public String nuevoCliente(Model model) {
+		Cliente cliente = new Cliente();
+		model.addAttribute("cliente", cliente);
+		model.addAttribute("titulo", "Nuevo cliente");
+		model.addAttribute("encabezado", "Nuevo Cliente");
+		model.addAttribute("boton", "Registrar Cliente");
+		return "nuevo-cliente";
 	}
 }
