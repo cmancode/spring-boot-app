a770f9e16d2905e86a73997549cdfbeadc7aba65
Carlos Alberto <carmantillab@gmail.com>
2017-12-14 21:55:16 -0500

    resourceHandler configuration. Listo (Class MvcConfig)

--- src/main/java/com/cmancode/clientes/app/controllers/ClienteController.java
+++ src/main/java/com/cmancode/clientes/app/controllers/ClienteController.java
@@ -12,13 +12,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -74,7 +78,7 @@ public class ClienteController {
 		return "nuevo-cliente";
 	}
 	@RequestMapping(value = "/cliente", method = RequestMethod.POST)
-	public String saveClient(@Valid Cliente cliente, @RequestParam("file") MultipartFile file, BindingResult result, RedirectAttributes flash, Model model) {
+	public String saveClient(@Valid Cliente cliente, BindingResult result, @RequestParam("file") MultipartFile file, RedirectAttributes flash, Model model) {
 		
 		if(result.hasErrors()) {
 			model.addAttribute("encabezado", "Nuevo Cliente");
@@ -84,8 +88,9 @@ public class ClienteController {
 		
 		if(!file.isEmpty()) {
 			//Se establece donde se almacenarán los recursos
-			Path directorioRecuros = Paths.get("src//main//resources//static/uploads");
-			String rootPath = directorioRecuros.toFile().getAbsolutePath();
+			//Path directorioRecuros = Paths.get("src//main//resources//static/uploads");
+			//String rootPath = directorioRecuros.toFile().getAbsolutePath();
+			String rootPath = "//home//carlos//Documentos//proyectos-sp//img-app//uploads";
 			try {
 				byte[] bytes = file.getBytes();
 				Path rutaCompleta = Paths.get(rootPath + "//" + file.getOriginalFilename());
